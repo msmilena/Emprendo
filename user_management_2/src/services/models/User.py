6a2014c5c1a76225bc3354b4bb849755ddb8cc9f
Bianca <bianca.romero.d97@gmail.com>
2024-10-06 23:31:25 -0500

    test

---
+++ user_management_2/src/services/models/User.py
@@ -0,0 +1,9 @@
+class User():
+
+    def __init__(self, idusuario, nombre, correo, password, tipo) -> None:
+        self.idusuario = idusuario
+        self.nombre = nombre
+        self.correo = correo
+        self.password = password
+        self.tipo = tipo
+        
---
+++ user_management_2/src/services/models/userRegister.py
@@ -0,0 +1,7 @@
+class UserRegister():
+
+    def __init__(self,  nombre, correo, password, tipo) -> None:
+        self.nombre = nombre
+        self.correo = correo
+        self.password = password
+        self.tipo = tipo
