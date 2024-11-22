733d354b52b82ec4fbd4cbb7f3d43dc882b164fc
Bianca <bianca.romero.d97@gmail.com>
2024-11-21 21:10:14 -0500

    get_favoritos

--- microservicios/valoracion-service/src/routes/ValoracionRoutes.py
+++ microservicios/valoracion-service/src/routes/ValoracionRoutes.py
@@ -84,11 +84,11 @@ def dlt_favoritos():
     else:
         return jsonify({'success': False, 'message': save_result['message']}), 400
 
-@main.route('/getFavoritos/usuario', methods=['POST'])
+@main.route('/getFavoritos/usuario', methods=['GET'])
 def get_favorito():
     idUsuario = request.args.get('idUsuario')
     try:
-        list_favoritos = ValoracionService.save_favorito(idUsuario)
+        list_favoritos = ValoracionService.get_favoritos(idUsuario)
         return jsonify({'success': True, 'valoraciones': list_favoritos})
     except CustomException as e:
         return jsonify({'message': str(e), 'success': False}), 500
