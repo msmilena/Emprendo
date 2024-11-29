190ba3a0a3bb7fb4423562075e9c0e7eeb306d43
Ivan Balandra Camacho <93243803+R-zor95@users.noreply.github.com>
2024-11-29 00:24:23 -0500

    Update App.js
    se reorganizo app.js

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,34 +1,30 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
+
+//pages
+import Home from "./pages/Home";
 import RegisterEmprendedor from "./pages/Register-Emprendedor";
 import RegisterUsuario from "./pages/RegisterUsuario";
 import TipoPerfil from "./pages/TipoPerfil";
-import Home from "./pages/Home";
-import DetalleEmprendimiento from "./pages/Detalle-Emprendimiento";
-import FavoritosUsuarios from "./pages/FavoritosUsuario";
-import HomeEmprendedor from "./pages/HomeEmprendedor";
-import ProductosEmprendedor from "./pages/ProductosEmprendedor";
-import EmprendimientoDatos from "./pages/EmprendimientoDatos";
-import PublicidadEmprendedor from "./pages/PublicidadEmprendedor";
-<<<<<<< Updated upstream
-import ProductFormPage from "./pages/ProductFormPage";
-import DetallesCuenta from "./pages/DetallesCuenta";
+import Login from "./pages/Login";
 import PerfilCliente from "./pages/PerfilCliente";
+import FavoritosUsuarios from "./pages/FavoritosUsuario";
 import ProductosxCategoria from "./pages/ProductosxCategoria";
 import EmprendimientosList from "./pages/EmprendimientosList";
-<<<<<<< Updated upstream
-=======
-import MiPerfilEmprendedor from "./pages/MiPerfilEmprendedor";
-
+import DetalleEmprendimiento from "./pages/Detalle-Emprendimiento";
+import EmprendimientoPorCategoria from "./pages/EmprendimientoPorCategoria"
 import RegisterPreferencias from "./pages/RegisterPreferencias";
 import CategoriaEmprendimiento from "./pages/CategoriaEmprendimiento";
+import HomeEmprendedor from "./pages/HomeEmprendedor";
+import ProductosEmprendedor from "./pages/ProductosEmprendedor";
+import ProductFormPage from "./pages/ProductFormPage";
+import EmprendimientoDatos from "./pages/EmprendimientoDatos";
+import PublicidadEmprendedor from "./pages/PublicidadEmprendedor";
+import DetallesCuenta from "./pages/DetallesCuenta";
 
->>>>>>> Stashed changes
-=======
-import EmprendimientoPorCategoria from "./pages/EmprendimientoPorCategoria"
->>>>>>> Stashed changes
+import MiPerfilEmprendedor from "./pages/MiPerfilEmprendedor";
+//
 import './App.css'
 
 function App() {
@@ -41,31 +37,23 @@ function App() {
         <Route path="/registerUsuario" element={<RegisterUsuario />} />
         <Route path="/rolSelect" element={<TipoPerfil />} />
         <Route path="/login" element={<Login />} />
-<<<<<<< Updated upstream
-        <Route path="/emprendimiento/:id" element={<DetalleEmprendimiento />} />
         <Route path="/perfilCliente" element={<PerfilCliente />} />
         <Route path="/favoritos/:id" element={< FavoritosUsuarios />} />
         <Route path="/categoria/:id" element={< ProductosxCategoria />} />
         <Route path="/listaEmprendimientos" element={< EmprendimientosList />} />
         <Route path="/detalleEmprendimiento/:id" element={< DetalleEmprendimiento />} />
         <Route path="/emprendimientoCategoria/:id" element={< EmprendimientoPorCategoria />} />
-        
-
-=======
-        <Route path="/favoritos/:id" element={< FavoritosUsuarios />} />
-        <Route path="/DetalleEmprendimiento/:id" element={< DetalleEmprendimiento />} />
         <Route path="/registerPreferencias" element={<RegisterPreferencias />} />
         <Route path="/categoriaEmprendimiento" element={<CategoriaEmprendimiento/>}/>
->>>>>>> Stashed changes
         <Route path="/homeEmprendedor/:id" element={< HomeEmprendedor />} />
         <Route path="/productosEmprendedor/:id" element={< ProductosEmprendedor />} />
         <Route path="/productosEmprendedor/nuevo" element={<ProductFormPage mode="new" />} />
         <Route path="/productosEmprendedor/ver/:id" element={<ProductFormPage mode="view" />} />
         <Route path="/productosEmprendedor/editar/:id" element={<ProductFormPage mode="edit" />} />
-
         <Route path="/emprendimientoDatos/:id" element={< EmprendimientoDatos />} />
         <Route path="/publicidadEmprendedor/:id" element={< PublicidadEmprendedor />} />
-        <Route path="/miPerfilEmprendedor/:id" element={< DetallesCuenta  />} />
+        <Route path="/detalleCuenta/:id" element={< DetallesCuenta  />} />
+        <Route path="/miPerfilEmprendedor/:id" element={< MiPerfilEmprendedor />} />
 
       </Routes>
     </Router>
